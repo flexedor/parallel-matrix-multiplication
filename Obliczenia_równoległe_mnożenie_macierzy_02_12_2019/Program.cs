bfaa135e734fec33c241953698f8e058d8149876
flexedor <fedor6574022@gmail.com>
2021-06-19 18:14:53 +0200

    first commit

---
+++ Obliczenia_równoległe_mnożenie_macierzy_02_12_2019/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obliczenia_równoległe_mnożenie_macierzy_02_12_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] result = new int[6, 5];
+            int[,] a = { { 4, 2, 8, 11, 55 }, { 5, 12, 23, 44, 1 },{12,53,53,55,453 },{ 12,543,54,55,11},{ 13,143,44,44,5} };
+            int[,] b = { { 3, 2 }, { 3, 3 }, { 51, 26 }, { 7, 11 }, { 9, 10 } };
+
+            Parallel.For(0, a.GetUpperBound(1), (int z) =>
+            {
+              
+                    for (int y = 0; y < a.GetUpperBound(z) / 2; y++)
+                    {
+                        for (int h = 0; h <= b.GetUpperBound(1); h++)
+                        {
+                            for (int g = 0; g <= a.GetUpperBound(1); g++)
+                            {
+                                lock (result)
+                                {
+                                    result[y, h] += a[y, g] * b[g, h];
+                                }
+                                }
+                        }
+                    }
+               
+                //if (z == 1)
+                //{
+                //    for (int y = a.GetUpperBound(0) / 2; y <= a.GetUpperBound(0); y++)
+                //    {
+                //        for (int h = 0; h <= b.GetUpperBound(1); h++)
+                //        {
+                //            for (int g = 0; g <= a.GetUpperBound(1); g++)
+                //            {
+                //                result[y, h] += a[y, g] * b[g, h];
+                //            }
+                //        }
+                //    }
+                //}
+
+            });
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"{result[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Obliczenia_równoległe_mnożenie_macierzy_02_12_2019/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Obliczenia_równoległe_mnożenie_macierzy_02_12_2019")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Obliczenia_równoległe_mnożenie_macierzy_02_12_2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f17182f3-db7e-433c-8328-3218906435a0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
